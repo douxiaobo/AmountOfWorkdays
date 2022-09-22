3678977f8a2029bbbcbbb74ff35fdbb3623a1039
Douxiaobo <dxb_1020@icloud.com>
2022-09-22 10:02:09 +0800

    In the Morning 2022/9/22 添加项目文件。

---
+++ ConsoleApp_LocalJson/Holidays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_LocalJson
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Holidays
+    {
+        [JsonProperty("code", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Code { get; set; }
+
+        [JsonProperty("holiday", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string,HolidayValue> Holiday { get; set; }
+    }
+
+    public partial class HolidayValue
+    {
+        [JsonProperty("holiday", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Holiday { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("after", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? After { get; set; }
+
+        [JsonProperty("wage", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Wage { get; set; }
+
+        [JsonProperty("target", NullValueHandling = NullValueHandling.Ignore)]
+        public string Target { get; set; }
+
+        [JsonProperty("date", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? Date { get; set; }
+    }
+}
---
+++ ConsoleApp_LocalJson/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp_LocalJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year:");
+            string year=Console.ReadLine();
+            Console.WriteLine("Month:");
+            string month=Console.ReadLine().PadLeft(2,'0');
+
+            string jsonfile = year + ".json";
+
+            System.IO.StreamReader sr = File.OpenText(jsonfile);
+            string json = sr.ReadToEnd();
+            Console.WriteLine("json=");
+            Console.WriteLine(json);
+
+            List<string> HolidayListInMonth = new List<string>();
+
+            var root=JsonConvert.DeserializeObject<Holidays>(json);
+
+            var keys = root.Holiday.Keys;
+            Console.WriteLine("Key=:");
+            foreach(var key in keys)
+            {
+                if(key.StartsWith(month))
+                {
+                    Console.WriteLine(key);
+                    HolidayListInMonth.Add(key);
+                }
+            }
+            Console.WriteLine("Value=:");
+            foreach(var value in HolidayListInMonth)
+            {
+                Console.WriteLine(value);
+            } 
+            Console.ReadKey();
+
+        }
+    }
+}
